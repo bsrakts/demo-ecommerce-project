d1207e64f273e3ffc80ae7a182971ec20d4e8f09
Büşra Aktaş <diba.aktas@ogr.sakarya.edu.tr>
2023-02-28 00:35:00 +0300

    feat(api): create product listing with single product api

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -11,3 +11,12 @@ export const productReducer = (state = initialState, { type, payload }) => {
       return state;
   }
 }
+
+export const selectedProductReducer = (state = {}, { type, payload }) => {
+  switch (type) {
+    case ActionTypes.SELECTED_PRODUCTS:
+      return { ...state, ...payload }
+    default:
+      return state;
+  }
+}
